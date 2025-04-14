5b745a4d0b6ed36b0d466e87a798ccc31615f1c0
Tiger <141835517+AdelTiger660@users.noreply.github.com>
2025-04-15 03:19:23 +0330

    Update pyhawk.py

--- pyhawk.py
+++ pyhawk.py
@@ -1,10 +1,10 @@
 import argparse
 from core import port_scanner
-
+from core import subdomain_finder
 def main():
     parser = argparse.ArgumentParser(description="PyHawk - Pentest CLI Toolkit")
     parser.add_argument('--portscan', metavar='HOST', help='Scan ports on a host')
-
+    parser.add_argument('--subdomains', metavar='DOMAIN', help='Find subdomains for a domain')
     args = parser.parse_args()
 
     if args.portscan:
@@ -12,3 +12,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+if args.subdomains:
+   
